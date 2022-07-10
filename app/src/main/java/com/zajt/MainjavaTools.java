8b37caefdd4d3982ca263ef1a7385d1915246054
MGLSIDE <qaesrd@126.com>
2022-07-10 21:07:02 +0800

    test

--- app/src/main/java/com/zajt/MainjavaTools.java
+++ app/src/main/java/com/zajt/MainjavaTools.java
@@ -2,7 +2,8 @@ package com.zajt;
 
 public class MainjavaTools
 {
-	static String copcopyright="zandrolua作者著佐权所有";
-	static  String  ZAndroluaVersion="0.0.7";
+	static String  CopyrightOwner="zandrolua作者著佐权所有";
+	static String  ZAndroluaVersion="0.0.7";
 	
+
 }
