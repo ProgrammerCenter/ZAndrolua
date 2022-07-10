92c5fef89f7dd7bfb5666ce05d1cc67c5531a5c9
MGLSIDE <qaesrd@126.com>
2022-07-10 22:20:42 +0800

    test

--- app/src/main/java/com/zajt/ZipIoTools.java
+++ app/src/main/java/com/zajt/ZipIoTools.java
@@ -34,22 +34,26 @@ class ZipIoTools
 			in.close();		
 		}
 		catch (Exception e)
-		{			e.printStackTrace();		
+		{
+			e.printStackTrace();		
 
 		}		
 		return retStr;
 	}
 
 	private static String getZipCommentFromBuffer(byte[] buffer, int len)
-	{			byte[] magicDirEnd = {0x50, 0x4b, 0x05, 0x06};			
+	{   byte[] magicDirEnd = {0x50, 0x4b, 0x05, 0x06};			
 		int buffLen = Math.min(buffer.length, len);		
 		//Check the buffer from the end	
 		for (int i = buffLen - magicDirEnd.length - 22; i >= 0; i--)
 		{				
 			boolean isMagicStart = true;		
 			for (int k=0; k < magicDirEnd.length; k++)
-			{					if (buffer[i + k] != magicDirEnd[k])
-				{					isMagicStart = false;					break;				
+			{	
+				if (buffer[i + k] != magicDirEnd[k])
+				{		
+					isMagicStart = false;				
+					break;				
 				}		
 			}				
 			if (isMagicStart)
@@ -57,10 +61,7 @@ class ZipIoTools
 				int commentLen = buffer[i + 20] + buffer[i + 22] * 256;				
 				int realLen = buffLen - i - 22;			
 
-				if (commentLen != realLen)
-				{					
 
-				}	
 				String comment = new String(buffer, i + 22, Math.min(commentLen, realLen));			
 				return comment;			
 			}		
